bef28503ee4247e9ba0fa5248734d40166fca51c
Nikitka <nik.nikitosa@mail.ru>
2025-02-23 21:09:23 +0300

    8 commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import (
